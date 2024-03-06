33659cea15e48bb4a47055585a85f3d102f504ed
Withoutwaxwqy <42163472+Withoutwaxwqy@users.noreply.github.com>
2024-03-06 03:42:31 +0000

    add some

--- calibration/accelerator/calibration.py
+++ calibration/accelerator/calibration.py
@@ -5,6 +5,7 @@ import matplotlib.ticker as mticker
 import matplotlib
 import numpy as np
 
+G = 9.7936174
 class INSCalibration:
   def __init__(self, herz):
     self.index = []
@@ -15,10 +16,26 @@ class INSCalibration:
     lw = 0.3
     gyrocolor, accecolor = 'red', 'blue'
     fig, axes = plt.subplots(3, 2, figsize=(20.5))
+    title = [[self.index[1], self.index[4]],
+             [self.index[2], self.index[5]],
+             [self.index[3], self.index[6]]]
+    latextitle = [['$x_{gyro}$', '$x_{acce}$'],
+                  ['$y_{gyro}$', '$y_{acce}$'],
+                  ['$z_{gyro}$', '$z_{acce}$']]
+    colorlist = [[gyrocolor, accecolor],
+                 [gyrocolor, accecolor],
+                 [gyrocolor, accecolor]]
+    for i in range(3):
+      for j in range(2):
+        axes[i,j].plot(self.data7[self.index[0]], self.data7[self.indexs[i+j*3+1]], linewidth=lw, c=colorlist[i][j])
+        axes[i,j].set_title(latextitle[i][j])
+        tick_spacing = self.data7.index.size / 5
+        axes[i, j].xaxis.set_major_locator(mticker.MultipleLocator(tick_spacing))
+    plt.show()
 
 
 
-def sixPositionMethod(xacce_vec,yacce_vec,zacce_vec,design_matrix=None,autodesign_matrix=False):
+def sixPositionMethod(xacce_vec,yacce_vec,zacce_vec,design_matrix=None,auto_design_matrix=False):
   """
   six position method to acceleration calibration
   :param xacce vec:l*n matrix X-axis acceleratoroutput
@@ -34,5 +51,21 @@ def sixPositionMethod(xacce_vec,yacce_vec,zacce_vec,design_matrix=None,autodesig
                  [0,0,0,0,G,-G],
                  [1, 1, 1, 1,1,1]])
   else:
-    A = desian_matrix
+    A = design_matrix
   AAA =A.T @(np.linalg.inv(A *A.T))
+
+def isEqualorNegative(vec, value, absolute_tolerance, continuity):
+  """
+  """
+  result = {"index":np.zeros(shape=vec.shape),
+            "EqualDuration":[],
+            "NegativeDuration":[]}
+  vec_e=np.isclose(vec.np.ones(shape=vec.shape)*value,    atol=absolute_tolerance)
+  vec_n=np.isclose(vec.np.ones(shape=vec.shape)*(-value), atol=absolute_tolerance)
+  result['index'] = vec_e * 1 + vec_n *(-1)
+  result['EqualDuration']    = continuousDuration(result['index'],  1, continuity)
+  result['NegativeDuration'] = continuousDuration(result['index'], -1, continuity)
+
+
+def continuousDuration(vec, value, threshold):
+  pass
